210e40dc3ce1e94e5b0c17b51d371815f1e1f23f
Victor Pereira <vpereirabr@gmail.com>
2015-07-03 10:45:05 +0200

    moved constants into module

--- src/lib/password/password.rb
+++ src/lib/password/password.rb
@@ -1,28 +1,29 @@
 require_relative 'endpoint'
 
-DEFAULT_POLICY = {
-  :retries => 3, # retry is reserved
-  :difok => 2,
-  :minlen => 9,
-  :dcredit => 1,
-  :ucredit => 1,
-  :lcredit => 1,
-  :ocredit => 1,
-  :minclass => 0,
-  :maxrepeat =>0,
-  :maxclassrepeat => 0,
-  :gecoscheck => 0,
-  :dicpath => ""
-
-}
-
-ENDPOINTS = {
-  :pam_d => "/etc/pam.d/common-auth",
-  :libpwquality => "/etc/security/pwquality.conf"
-}
 
 
 module Password
+
+  DEFAULT_POLICY = {
+    :retries => 3, # retry is reserved
+    :difok => 2,
+    :minlen => 9,
+    :dcredit => 1,
+    :ucredit => 1,
+    :lcredit => 1,
+    :ocredit => 1,
+    :minclass => 0,
+    :maxrepeat =>0,
+    :maxclassrepeat => 0,
+    :gecoscheck => 0,
+    :dicpath => ""
+
+  }
+
+  ENDPOINTS = {
+    :pam_d => "/etc/pam.d/common-auth",
+    :libpwquality => "/etc/security/pwquality.conf"
+  }
 
   class Password
     attr_reader :policy, :endpoints
